2ed94773e49696d8049cfe84b92c6cafcd311d6e
Flaviagab <flaviagabriella.cs@gmail.com>
2026-08-15 16:50:21 -0300

    feat: instalação do nativewind e começo da tela inicial

---
+++ app/index.tsx
@@ -0,0 +1,28 @@
+
+import { ScrollView, StyleSheet, View } from "react-native";
+
+export default function Index() {
+
+  return (
+
+      <ScrollView>
+        <View
+          style={
+            [styles.container]
+          }>
+            
+      
+
+        </View>
+      </ScrollView>
+
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  }
+})
